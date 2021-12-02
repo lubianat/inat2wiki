05783484d96f36cef2d28873034fbdd58bb7f443
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2021-12-02 08:55:19 -0300

    Improve URL building for special upload

--- parse_observation.py
+++ parse_observation.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import requests
+import urllib.parse
+
 
 id = input("Enter the iNaturalist observation id:")
 
@@ -25,6 +27,10 @@ upload_params["user_name"] = observation_data["user"]["name"]
 upload_params["date"] = observation_data["observed_on"]
 upload_params["taxon"] = observation_data["taxon"]["name"]
 
+
+switcher = {"cc-by": "cc-by-4.0", "cc-by-sa": "cc-by-sa-4.0", "cc0": "Cc-zero"}
+
+
 print("https://commons.wikimedia.org/wiki/Special:Upload")
 
 
@@ -32,32 +38,41 @@ print("====== Photo URL =====")
 print(photo_url)
 
 print("====== Title ======")
-print(upload_params["taxon"] + " " + str(upload_params["photo_id"]) + ".jpeg")
+title = upload_params["taxon"] + " " + str(upload_params["photo_id"]) + ".jpeg"
+
+print(title)
 
 print("====== Summary ======")
-print(
-    """
-{{Information
-|description={{en|"""
-    + upload_params["taxon"]
-    + """}}
-|date="""
-    + upload_params["date"]
-    + """
-|source=https://www.inaturalist.org/photos/"""
-    + str(upload_params["photo_id"])
-    + """
-|author=[https://www.inaturalist.org/users/"""
-    + str(upload_params["user_id"])
-    + " "
-    + upload_params["user_name"]
-    + """ ]
+license = switcher[upload_params["photo_license"]]
+print("License: " + license)
+summary = (
+    """{{Information"""
+    + f"""
+|description={upload_params["taxon"]}
+|date={upload_params["date"]}
+|source=https://www.inaturalist.org/photos/{str(upload_params["photo_id"])}
+|author=[https://www.inaturalist.org/users/{str(upload_params["user_id"])} {upload_params["user_name"]}]
 |permission=
 |other versions=
-}}
-
-[[Category:"""
-    + upload_params["taxon"]
-    + """]]
 """
+    + """}}
+    
+{{"""
+    + f"iNaturalist|{id}"
+    + "}}"
+    + f"""
+
+[[Category:{upload_params["taxon"]}]]"""
 )
+
+
+print(summary)
+upload_page = "https://commons.wikimedia.org/wiki/Special:Upload"
+
+summary_for_url = urllib.parse.quote(summary)
+title_for_url = urllib.parse.quote(title)
+upload_url = (
+    upload_page
+    + f"?wpUploadDescription={summary_for_url}&wpLicense={license}&wpDestFile={title_for_url}&wpSourceType=url&wpUploadFileURL={photo_url}"
+)
+print(upload_url)
