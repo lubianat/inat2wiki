ce4e624fc725c730345f44c9d80fc02c18f23bdf
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2023-08-17 09:45:46 -0300

    Add basic error handling

--- www/python/src/app.py
+++ www/python/src/app.py
@@ -211,3 +211,11 @@ class iNaturalistSearchConfiguration:
             starting_page=self.page,
             langcode_list=self.langcodes.split(","),
         )
+
+@app.errorhandler(404)
+def not_found_error(error):
+  return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+  return render_template('500.html'), 500
